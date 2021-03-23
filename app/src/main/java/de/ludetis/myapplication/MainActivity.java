7ce9fa8bd7654a4ff906c91e8cbd4f354cb2ab68
Your Name <you@example.com>
2021-03-23 20:05:01 +0200

    permissions native

--- app/src/main/java/de/ludetis/myapplication/MainActivity.java
+++ app/src/main/java/de/ludetis/myapplication/MainActivity.java
@@ -3,7 +3,10 @@ package de.ludetis.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.app.Dialog;
 import android.app.LoaderManager;
@@ -11,6 +14,7 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -38,104 +42,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewById = findViewById(R.id.textView);
 
-        registerForContextMenu(viewById);
+        int cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
 
-//        Toast toast = Toast.makeText(this, "Текст для тоста!", Toast.LENGTH_SHORT);
-//        toast.show();
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                //Свой диалог "Зачем давать пермишин"
+                new AlertDialog.Builder(this)
+                        .setTitle("Свой диалог")
+                        .setMessage("Зачем давать пермишин")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String[] permissions = {Manifest.permission.CAMERA};
+                                ActivityCompat.requestPermissions(MainActivity.this, permissions, 777);
+                            }
+                        }).create().show();
 
-//        Snackbar.make(findViewById(R.id.root_view), "Текст для тоста!", Snackbar.LENGTH_LONG).show();
-
-
-
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this)
-//                .setTitle("Title of dialog")
-//                .setMessage("Message of Dialog")
-//                .setIcon(R.mipmap.ic_launcher)
-////                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
-////                    @Override
-////                    public void onClick(DialogInterface dialog, int which) {
-////                        dialog.dismiss();
-////                        Toast toast = Toast.makeText(MainActivity.this, "Вы нажали НЕТ!", Toast.LENGTH_SHORT);
-////                        toast.show();
-////                    }
-////                })
-//                .setPositiveButton("Принял", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        dialog.dismiss();
-//                        Toast toast = Toast.makeText(MainActivity.this, "Вы нажали Принял!", Toast.LENGTH_SHORT);
-//                        toast.show();
-//                    }
-//                })
-//                .setOnCancelListener(new DialogInterface.OnCancelListener() {
-//                    @Override
-//                    public void onCancel(DialogInterface dialog) {
-//                        Toast toast = Toast.makeText(MainActivity.this, "Диалог был отменен!", Toast.LENGTH_SHORT);
-//                        toast.show();
-//                    }
-//                })
-//                .setCancelable(false);
-//
-//        AlertDialog alertDialog = builder.create();
-//
-//        alertDialog.show();
-
-
-//        MyDialog myDialog = new MyDialog(this);
-//        myDialog.show();
-
-    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater menuInflater = getMenuInflater();
-//        menuInflater.inflate(R.menu.my_menu, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.menu_about :
-//                Toast.makeText(this, "Вы выбрали " + item.getTitle(), Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.menu_check_update :
-//                Toast.makeText(this, "Вы нажали " + item.getTitle(), Toast.LENGTH_SHORT).show();
-//                return true;
-//            case R.id.menu_donat :
-//                Toast.makeText(this, "Вы тапнули " + item.getTitle(), Toast.LENGTH_SHORT).show();
-//                return true;
-//
-//            default: return super.onOptionsItemSelected(item);
-//        }
-//    }
-
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        menu.add(Menu.NONE, 0, Menu.NONE, "Пункт 0");
-        menu.add(Menu.NONE, 1, Menu.NONE, "Пункт 1");
-        menu.add(Menu.NONE, 2, Menu.NONE, "Пункт 2");
-        menu.add(Menu.NONE, 3, Menu.NONE, "Пункт 3");
-    }
-
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case 0 :
-                Toast.makeText(this, "Вы выбрали " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-            case 1 :
-                Toast.makeText(this, "Вы нажали " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-            case 2 :
-                Toast.makeText(this, "Вы тапнули " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-            case 3 :
-                Toast.makeText(this, "Вы ткнули " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-
-            default: return super.onOptionsItemSelected(item);
+            } else {
+                String[] permissions = {Manifest.permission.CAMERA};
+                ActivityCompat.requestPermissions(this, permissions, 777);
+            }
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 777) {
+            if (permissions.length == 1 && permissions[0].equals(Manifest.permission.CAMERA) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            }
+        }
+    }
+
+    private void openCamera() {
+        Toast.makeText(this, "openCamera()", Toast.LENGTH_LONG).show();
+    }
+
 }
